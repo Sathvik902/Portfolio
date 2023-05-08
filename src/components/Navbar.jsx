f062b9e4634e587abb2f9feeb4732c317aa92e53
Sathvik K <sathvik902@gmail.com>
2023-05-08 23:10:43 +0530

    second

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
           window.scrollTo(0,0);
         }}>
           <img src = {logo} alt="Logo" className='w-9 h-9 object-contain '/>
-          <p className='text-white text-[18px] font-bold cursor-pointer flex'>Sathvik😜<span className='sm:block hidden'></span></p>
+          <p className='text-white text-[18px] font-bold cursor-pointer flex'>Sathvik😀<span className='sm:block hidden'></span></p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
           {navLinks.map((link) => (
