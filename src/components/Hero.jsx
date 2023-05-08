82513d96db7917ad5c9c5d7fb1cd4d1f32cb1b20
Sathvik K <sathvik902@gmail.com>
2023-05-08 23:36:54 +0530

    second2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,14 +8,18 @@ const Hero = () => {
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
-          <div className='w-1 sm:h-80 h-40 violet-gradient'/>
+          <div className='w-1 sm:h-80 h-100 violet-gradient'/>
         </div>
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Sathvik</span></h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>A passionate MERN developer, ML and DL enthusiast and profecient in Data Structures and Algorithms.</p>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>A passionate MERN Stack developer, ML and DL enthusiast and profecient in Data Structures and Algorithms.
+          Student of Bangalore Institute of technology.</p>
+          <br/>
+          <br/>
+          <h6 className={`${styles.heroSubText} text-white`}>WebDev|Open Source|ML</h6>
         </div>
       </div>
-      <ComputersCanvas/>
+      
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href="#about">
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
